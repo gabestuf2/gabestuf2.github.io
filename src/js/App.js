21fae35105bd7c758654274089e32efc30f06df4
BuildTools <unconfigured@null.spigotmc.org>
2021-04-01 23:06:55 -0400

    react test 3

---
+++ src/js/App.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+//Importing Components 
+
+function App() {
+
+    return (
+        <div className='App'>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default App;

1a501596b2c26fccd1c3996867ca97272464ee9a
BuildTools <unconfigured@null.spigotmc.org>
2021-04-01 22:56:32 -0400

    react test 1

--- index.js
+++
@@ -1,6 +0,0 @@
-const nameBtn = document.getElementById('name');
-const paragraph = document.getElementById('paragraph');
-
-nameBtn.addEventListener('click', function () {
-    paragraph.innerHTML = 'poop ' + paragraph.innerHTML;
-})

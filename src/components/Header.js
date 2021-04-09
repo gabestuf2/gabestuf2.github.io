98bb98a4628b916c7bf91a2405c46caf60e1b53b
Gabe <gcamacho1258@gmail.com>
2021-04-09 16:51:12 -0400

    reaccccttt

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+       <div className="headerContainer">
+
+           <h1>HEADER</h1>
+     
+       </div>
+     );
+   }
+   
+   export default Header;
+   
